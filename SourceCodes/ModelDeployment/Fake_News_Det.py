9978c4d773238f95b90ef4ea782e0acf690ddfdf
sobz2019 <sobz87@gmail.com>
2022-08-17 12:06:28 +0100

    Updated

--- SourceCodes/ModelDeployment/Fake_News_Det.py
+++ SourceCodes/ModelDeployment/Fake_News_Det.py
@@ -72,64 +72,6 @@ preprocessor = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_prep
 # bert encoder - https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-12_H-512_A-8/2
 encoder = hub.KerasLayer("https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-12_H-512_A-8/2",trainable=True)
 
-# MAX_SEQ_LEN=256
-# input_word_ids = tf.keras.layers.Input(shape=(MAX_SEQ_LEN,), dtype=tf.int32,name="input_word_ids")
-# input_mask = tf.keras.layers.Input(shape=(MAX_SEQ_LEN,), dtype=tf.int32,name="input_mask")
-# segment_ids = tf.keras.layers.Input(shape=(MAX_SEQ_LEN,), dtype=tf.int32,name="segment_ids")
-
-
-# def get_masks(tokens, max_seq_length):
-    # return [1]*len(tokens) + [0] * (max_seq_length - len(tokens))
-
-# def get_segments(tokens, max_seq_length):
-    # """Segments: 0 for the first sequence, 1 for the second"""
-    # segments = []
-    # current_segment_id = 0
-    # for token in tokens:
-        # segments.append(current_segment_id)
-        # if token == "[SEP]":
-            # current_segment_id = 1
-    # return segments + [0] * (max_seq_length - len(tokens))
-
-# pooled_output, sequence_output = bert_layer([input_word_ids, input_mask, segment_ids])
-
-# #Create the tokenizer with the BERT layer and import it tokenizer using the original vocab file.
-# FullTokenizer=bert.bert_tokenization.FullTokenizer
-
-# vocab_file=bert_layer.resolved_object.vocab_file.asset_path.numpy()
-
-# do_lower_case=bert_layer.resolved_object.do_lower_case.numpy()
-
-# tokenizer=FullTokenizer(vocab_file,do_lower_case)
-
-# def get_ids(tokens, tokenizer, max_seq_length):
-    # """Token ids from Tokenizer vocab"""
-    # token_ids = tokenizer.convert_tokens_to_ids(tokens,)
-    # input_ids = token_ids + [0] * (max_seq_length-len(token_ids))
-    # return input_ids
-
-# def create_single_input(sentence,MAX_LEN):
-    # stokens = tokenizer.tokenize(sentence)
-    # stokens = stokens[:MAX_LEN]
-    # stokens = ["[CLS]"] + stokens + ["[SEP]"]
- 
-    # ids = get_ids(stokens, tokenizer, MAX_SEQ_LEN)
-    # masks = get_masks(stokens, MAX_SEQ_LEN)
-    # segments = get_segments(stokens, MAX_SEQ_LEN)
-
-    # return ids,masks,segments
-
-# def create_input_array(sentences):
-    # input_ids, input_masks, input_segments = [], [], []
-    # for sentence in tqdm(sentences,position=0, leave=True):
-        # ids,masks,segments=create_single_input(sentence,MAX_SEQ_LEN-2)
-        # input_ids.append(ids)
-        # input_masks.append(masks)
-        # input_segments.append(segments)
-    # return [np.asarray(input_ids, dtype=np.int32), np.asarray(input_masks, dtype=np.int32), np.asarray(input_segments, dtype=np.int32)]
-
-## Model Training
-
 
 print('Models Loaded')
 
